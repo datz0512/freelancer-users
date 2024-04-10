195bf74d4132017d48406b18a5862b1e1bb235b8
datz <datblu2003@gmail.com>
2024-04-11 01:17:35 +0700

    first commit

---
+++ src/queues/user.consumer.ts
@@ -0,0 +1,47 @@
+import { IBuyerDocument, winstonLogger } from '@datz0512/freelancer-shared';
+import { config } from '@users/config';
+import { Channel, ConsumeMessage, Replies } from 'amqplib';
+import { Logger } from 'winston';
+import { createConnection } from '@users/queues/connection';
+import { createBuyer, updateBuyerPurchasedGigsProp } from '@users/services/buyer.service';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'usersServiceConsumer', 'debug');
+
+const consumeBuyerDirectMessage = async (channel: Channel): Promise<void> => {
+  try {
+    if (!channel) {
+      channel = (await createConnection()) as Channel;
+    }
+    const exchangeName = 'freelancer-buyer-update';
+    const routingKey = 'user-buyer';
+    const queueName = 'user-buyer-queue';
+    await channel.assertExchange(exchangeName, 'direct');
+    const freelancerQueue: Replies.AssertQueue = await channel.assertQueue(queueName, {
+      durable: true,
+      autoDelete: false
+    });
+    await channel.bindQueue(freelancerQueue.queue, exchangeName, routingKey);
+    channel.consume(freelancerQueue.queue, async (msg: ConsumeMessage | null) => {
+      const { type } = JSON.parse(msg!.content.toString());
+      if (type === 'auth') {
+        const { username, email, profilePicture, country, createdAt } = JSON.parse(msg!.content.toString());
+        const buyer: IBuyerDocument = {
+          username,
+          email,
+          profilePicture,
+          country,
+          purchasedGigs: [],
+          createdAt
+        };
+        await createBuyer(buyer);
+      } else {
+        const { buyerId, purchasedGigs } = JSON.parse(msg!.content.toString());
+        await updateBuyerPurchasedGigsProp(buyerId, purchasedGigs, type);
+      }
+    });
+  } catch (error) {
+    log.log('error', 'UsersService consumeBuyerDirectMessage() method error:', error);
+  }
+};
+
+export { consumeBuyerDirectMessage };
